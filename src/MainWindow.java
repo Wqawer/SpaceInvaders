e2a7d06fe80e9b6b0b4ce926a5226bcff10c4d77
Wqawer <m.wqawer@gmail.com>
2020-04-25 13:41:30 +0200

    for git

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -44,11 +44,8 @@ public class MainWindow extends Frame {
         add(panel);
         Button startButton = new Button("New Game");
         startButton.setBounds(200, 60, 100, 60);
-        startButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                startGame(panel);
-            }
+        startButton.addActionListener((e) -> {
+            startGame(panel);
         });
         panel.add(startButton);
 
